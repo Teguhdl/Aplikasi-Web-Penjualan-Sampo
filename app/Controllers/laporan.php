84e391b18bdb1bc99ae2b677ff29760d6c0be69e
Teguhdl <107293403+Teguhdl@users.noreply.github.com>
2023-01-17 20:54:48 +0700

    Add files via upload

--- app/Controllers/laporan.php
+++ app/Controllers/laporan.php
@@ -7,19 +7,49 @@ use App\Models\JurnalModel;
 
 class Laporan extends BaseController
 {
-    public function viewjurnalumum(){ 
+    // public function viewjurnalumum(){ 
 
+    //     $jurnalmodel = model(JurnalModel::class);
+    //     $data = $jurnalmodel->getjurnal();
+    //     echo view('template/headertemplate');
+    //     echo view('template/sidebartemplate');
+    //     echo view('Laporan/JurnalUmum',
+    //                 [
+    //                     'title' => 'Laporan Jurnal Umum',
+    //                     'data' => $data,
+    //                 ]
+    //             );      
+    //             echo view('template/footertemplate');    
+    // }
+
+    public function index()
+    {
         $jurnalmodel = model(JurnalModel::class);
-        $data = $jurnalmodel->getjurnal();
-        echo view('template/headertemplate');
-        echo view('template/sidebartemplate');
-        echo view('Laporan/JurnalUmum',
-                    [
-                        'title' => 'Laporan Jurnal Umum',
-                        'data' => $data,
-                    ]
-                );      
-                echo view('template/footertemplate');    
+        if(isset($_POST['submit']))
+        {
+            $tanggal1 = $this->request->getPost('tanggal1');
+            $tanggal2 = $this->request->getPost('tanggal2');
+            $data = $jurnalmodel->laporan_periode($tanggal1,$tanggal2);
+           echo view('template/headertemplate');
+           echo view('template/sidebartemplate');
+           echo view('laporan/JurnalUmum',[
+            'title' => 'Laporan Jurnal Umum',
+              'data' => $data,
+          ]);
+           echo view('template/footertemplate');
+        }
+        else 
+        {
+      $jurnalmodel = model(JurnalModel::class);
+      $data= $jurnalmodel->laporan_default();
+       echo view('template/headertemplate');
+       echo view('template/sidebartemplate');
+       echo view('laporan/JurnalUmum', [
+                           'title' => 'Laporan Jurnal Umum',
+                             'data' => $data,
+                         ]);
+       echo view('template/footertemplate');
+        }
     }
 } 
 
