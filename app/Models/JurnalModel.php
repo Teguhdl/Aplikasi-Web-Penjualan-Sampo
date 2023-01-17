16fd7adbbbc3558be76a68a75b1d6ee45e7e08a5
Teguhdl <107293403+Teguhdl@users.noreply.github.com>
2023-01-17 20:55:38 +0700

    Add files via upload

--- app/Models/JurnalModel.php
+++ app/Models/JurnalModel.php
@@ -10,7 +10,23 @@ class JurnalModel extends Model
     protected $primaryKey ='id_jurnal';
     protected $allowedFields = ['id_jurnal', 'kode_akun','nama_akun', 'tgl_jurnal', 'posisi_d_c', 'nominal','transaksi'];
 
-    public function getjurnal(){
-        return $this->findAll();
+  
+    function laporan_periode($tanggal1,$tanggal2)
+    {
+        $db = db_connect();
+        $query=$db->query("SELECT * FROM jurnal
+        WHERE tgl_jurnal between '$tanggal1' and '$tanggal2'
+        group by id_jurnal");
+        $result = $query->getresult('array');
+        return $result;
     }
+    function laporan_default()
+    {
+        $db = db_connect();
+        $query=$db->query('SELECT * FROM jurnal
+        group by id_jurnal');
+        $result = $query->getresult('array');
+        return $result;
+    }
+
 } 
